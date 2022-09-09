7de415185ea7560c9f74983a0f8bec15b046e83b
AdegokeBabatunde43 <101894263+AdegokeBabatunde43@users.noreply.github.com>
2022-09-09 15:03:21 +0100

    adding whitenoise to settings

--- login/settings.py
+++ login/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,3 +140,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
