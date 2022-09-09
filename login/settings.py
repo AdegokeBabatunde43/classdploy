bf6284077b6f6814859c08224691486b7207bccb
AdegokeBabatunde43 <101894263+AdegokeBabatunde43@users.noreply.github.com>
2022-09-09 15:09:58 +0100

    adding whitenoise to settings

--- login/settings.py
+++ login/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'corsheaders',
-    'pages',
     'todo',
     'rest_framework',
 ]
@@ -131,9 +130,7 @@ CORS_ALLOWED_ORIGINS = [
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
